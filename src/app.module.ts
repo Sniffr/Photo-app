53f6a1343788f0dda351692c55f8d001c8de32cc
Sidney wesonga (Youth 4 tech) <54477693+Sniffr@users.noreply.github.com>
2025-01-17 11:45:00 +0300

    chore: Add initial dependencies and TypeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { PhotoModule } from './photo/photo.module';
+import { FeedModule } from './feed/feed.module';
+import { InteractionModule } from './interaction/interaction.module';
+import { SearchModule } from './search/search.module';
+import { NotificationModule } from './notification/notification.module';
+import * as entities from './domain/entities';
+import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'photo_inc',
+      entities: Object.values(entities) as EntityClassOrSchema[],
+      synchronize: process.env.NODE_ENV !== 'production',
+    }),
+    AuthModule,
+    UserModule,
+    PhotoModule,
+    FeedModule,
+    InteractionModule,
+    SearchModule,
+    NotificationModule,
+  ],
 })
 export class AppModule {}
